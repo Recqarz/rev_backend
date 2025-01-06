2b37800474505d58c667a35e67e5b40d780bdc0a
ranjan-recqarz <ranjan.yadav@recqarz.com>
2025-01-06 11:32:49 +0530

    folder stracture has been created

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
-    return res.status(200).send({message:"data fetched successfull"})
+  return res.status(200).send({ message: "data fetched successfull" });
 });
 
 app.listen(PORT, () => {
