cd6d251efa8c35aae5105f932f21e5548ba83db5
ranjan-recqarz <ranjan.yadav@recqarz.com>
2025-02-06 12:16:36 +0530

    API has been created for update profile picture

--- src/routes/commonRoute.js
+++ src/routes/commonRoute.js
@@ -7,7 +7,13 @@ const { updateUser } = require("../controllers/common/updateUser");
 const authMiddleware = require("../middlewares/authMiddleware");
 const { sendOTP, verifyOTP } = require("../utils/otp");
 const { resetPassword } = require("../controllers/common/forgotPassword");
-const { getProfileDetails } = require("../controllers/common/getProfileDetails");
+const {
+  getProfileDetails,
+} = require("../controllers/common/getProfileDetails");
+const {
+  updateProfilePicture,
+} = require("../controllers/common/updateProfilePicture");
+const { upload } = require("../middlewares/multer.middleware");
 
 const commonRoute = express.Router();
 
@@ -15,6 +21,12 @@ commonRoute.get("/details", authMiddleware, getProfileDetails);
 commonRoute.post("/login-and-send-otp", loginAndSendOtp);
 commonRoute.post("/verify-otp-and-generate-token", verifyOtpAndGenerateToken);
 commonRoute.patch("/update", authMiddleware, updateUser);
+commonRoute.patch(
+  "/update-profile-picture",
+  authMiddleware,
+  upload.single("avatar"),
+  updateProfilePicture
+);
 commonRoute.post("/send-otp", sendOTP);
 commonRoute.post("/verify-otp", verifyOTP);
 commonRoute.patch("/forgot-password/reset-password", resetPassword);
