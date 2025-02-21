89f47cbb3f7216040f995150c0cbc6fd9efed8ca
ranjan-recqarz <ranjan.yadav@recqarz.com>
2025-02-21 18:04:50 +0530

    API has been created for supervisor getCaseAndFieldExecutiveFomData

--- src/controllers/supervisor/getAllSupervisorAssignCase.js
+++ src/controllers/supervisor/getAllSupervisorAssignCase.js
@@ -1,5 +1,3 @@
-const mongoose = require("mongoose");
-const PropertyDetailsModel = require("../../models/propertyDetailsByFieldExecutiveModel");
 const CaseModel = require("../../models/caseModel");
 
 const getSupervisorCase = async (req, res) => {
--- src/routes/supervisorRoute.js
+++ src/routes/supervisorRoute.js
@@ -2,9 +2,11 @@ const express = require("express");
 const {
   getSupervisorCase,
 } = require("../controllers/supervisor/getAllSupervisorAssignCase");
+const getCaseAndFieldExecutiveFomData = require("../controllers/supervisor/getCaseAndFieldExecutiveFomData");
 
 const superRoute = express.Router();
 
 superRoute.get("/cases", getSupervisorCase);
+superRoute.get("/case/:caseId", getCaseAndFieldExecutiveFomData);
 
 module.exports = { superRoute };
