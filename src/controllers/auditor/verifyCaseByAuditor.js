7371e005b43341767d0b2c10175f204b25a2e7f4
sumansauravnov <suman.saurav@recqarz.com>
2025-02-26 15:57:36 +0530

    auditor api for case details

---
+++ src/controllers/auditor/verifyCaseByAuditor.js
@@ -0,0 +1,32 @@
+const CaseModel = require("../../models/caseModel");
+
+const verifyCaseByAuditor = async (req, res) => {
+  try {
+    const userId = req?.user?._id;
+    const { caseId } = req.params;
+    if (!userId) {
+      return res.status(400).send({ error: "Please provide user-id" });
+    }
+    if (!caseId) {
+      return res.status(400).send({ error: "Please provide case-id" });
+    }
+    const updatedCase = await CaseModel.findByIdAndUpdate(
+      caseId,
+      {
+        auditorId: userId,
+        "verifiedBy.auditor": true,
+      },
+      { new: true }
+    );
+    if (!updatedCase) {
+      return res.status(404).send({ error: "Case not found or not updated" });
+    }
+    return res
+      .status(200)
+      .send({ message: "Case updated successfully!", updatedCase });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { verifyCaseByAuditor };
